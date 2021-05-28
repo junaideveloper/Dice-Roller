8a0d8bf7b1e06f90c0be23e6d6cdedb6b484bfa4
junaideveloper@gmail.com <you@example.com>
2021-05-28 15:32:34 +0400

    dice roll image added

--- app/src/main/java/com/junaidtt/diceroller/MainActivity.kt
+++ app/src/main/java/com/junaidtt/diceroller/MainActivity.kt
@@ -2,8 +2,7 @@ package com.junaidtt.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -30,12 +29,23 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
-        //update the screen with dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-        val Toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
-        Toast.show()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        diceImage.contentDescription = diceRoll.toString()
     }
+
 
 }
 
